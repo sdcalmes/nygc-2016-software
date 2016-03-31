55c70e8633e4387e7984d4035f31423dbaba2b41
Sam Calmes <sdcalmes@gmail.com>
2016-03-31 13:57:26 -0500

    Header comments, adding preproccessor includes

--- nygc_2016.c
+++ nygc_2016.c
@@ -1,1 +1,29 @@
+/*
+# ############################################################################ #
+#									       #
+# Authors: Sam Calmes, Tara Abernathy, Tofe Salako, Jake Schieber              #
+#                                                                              #
+# NYGC Main program. Handles motor,magnet, bluetooth, etc logic. Sends data    #
+# over bluetooth to mobile phone.                                              #
+#									       #
+# ############################################################################ #
+*/
 
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("Welcome to NYGC.\n");
+
+}
